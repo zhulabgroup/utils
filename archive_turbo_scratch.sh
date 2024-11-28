21fe756e069e12931824d5f5d0f335b7602f97b0
Kai Zhu <kai.zhu@alumni.duke.edu>
2024-11-28 17:53:31 -0500

    Add shell script to archive on Turbo

---
+++ archive_turbo_scratch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=21
+#SBATCH --mem-per-cpu=8g
+#SBATCH --time=24:00:00
+#SBATCH --account=zhukai0
+#SBATCH --partition=standard
+#SBATCH --mail-type=BEGIN,END
+
+cd /nfs/turbo/seas-zhukai/archives/${AF}
+module load archivetar
+archivetar --prefix ${AF} --zstd --bundle-path /scratch/zhukai_root/zhukai0/${UN} --size 100G
+
+# Download this bash script to your home directory
+# Make it executable
+# chmod +x archive-turbo-scratch.sh
+# Use it with the following command
+# sbatch --export=AF='<target folder>',UN=<unique name> --job-name='<job name>' archive-turbo-scratch.sh
+# After Slurm job is completed, copy tar files from scratch to data den on Globus
+# Once transfer to data den on Globus is completed, feel free to delete related files from turbo and scratch
